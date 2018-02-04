099990f7f108e7a1d82c76d7abf2acbccc3ec51f
Ivan Shastin <ivan.shastin@concur.com>
2018-02-04 13:07:14 +0100

    autoprefixer & cssnano

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var postcss = require('gulp-postcss');
+var gulp = require('gulp');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+
+gulp.task('serve', ['css'], function() {
+    gulp.watch("./css/*.css", ['css']);
+});
+
+gulp.task('css', function() {
+    var plugin = [
+        autoprefixer(),
+        cssnano()
+    ];
+    return gulp.src('./css/*.css')
+        .pipe(postcss(plugin))
+        .pipe(gulp.dest('./dest'));
+})
+
+gulp.task('default', ['serve']);
